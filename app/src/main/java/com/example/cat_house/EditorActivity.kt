343a13f50f1363f456768ff83ab122ee1fceafc4
Roman <Roman444.zaharov@gmail.com>
2021-11-17 20:44:58 +0300

    Удаление, вставка, изменение ДБ

--- app/src/main/java/com/example/cat_house/EditorActivity.kt
+++ app/src/main/java/com/example/cat_house/EditorActivity.kt
@@ -1,13 +1,15 @@
 package com.example.cat_house
 
+import android.content.ContentValues
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.TextUtils
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cat_house.data.HotelContract
+import com.example.cat_house.data.HotelDbHelper
 import com.example.cat_house.databinding.ActivityEditorBinding
 
 class EditorActivity : AppCompatActivity() {
@@ -24,6 +26,76 @@ class EditorActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupSpinner();
+
+        binding.insertGuest.setOnClickListener {
+            insertGuest()
+            finish()
+        }
+
+        binding.updateGuest.setOnClickListener {
+            updateGuest()
+            finish()
+        }
+
+        binding.deleteGuest.setOnClickListener {
+            deleteGuest()
+            finish()
+        }
+    }
+
+    private fun deleteGuest() {
+        val dbHelper = HotelDbHelper(this)
+        val db = dbHelper.writableDatabase
+
+        db.delete(HotelContract.GuestEntry.TABLE_NAME, "${HotelContract.GuestEntry.COLUMN_NAME}= ?", arrayOf("wer"))
+    }
+
+    private fun updateGuest() {
+        val dbHelper = HotelDbHelper(this)
+        val db = dbHelper.writableDatabase
+
+        val values = ContentValues()
+        values.apply {
+            put(HotelContract.GuestEntry.COLUMN_NAME, "Mashka")
+            put(HotelContract.GuestEntry.COLUMN_CITY, "NewYork")
+        }
+
+        val updatedRow = db.update(
+            HotelContract.GuestEntry.TABLE_NAME,
+            values,
+            "${HotelContract.GuestEntry.COLUMN_NAME}= ?",
+            arrayOf("Mashka")
+        )
+    }
+
+    private fun insertGuest() {
+        val name = binding.editGuestName.text.toString().trim()
+        val city = binding.editGuestCity.text.toString().trim()
+        val age = binding.editGuestAge.text.toString().trim().toInt()
+
+        val dbHelper = HotelDbHelper(this)
+
+        val values = ContentValues()
+        values.apply {
+            put(HotelContract.GuestEntry.COLUMN_NAME, name)
+            put(HotelContract.GuestEntry.COLUMN_CITY, city)
+            put(HotelContract.GuestEntry.COLUMN_GENDER, gender)
+            put(HotelContract.GuestEntry.COLUMN_AGE, age)
+        }
+
+        val db = dbHelper.writableDatabase
+
+        val newRowId = db.insert(HotelContract.GuestEntry.TABLE_NAME, null, values)
+
+        when (newRowId) {
+            -1L -> {
+                Toast.makeText(this, "Ошбика регистрации гостя", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                Toast.makeText(this, "Гость зарегестрирован", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     private fun setupSpinner() {
--- app/src/main/java/com/example/cat_house/MainActivity.kt
+++ app/src/main/java/com/example/cat_house/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.cat_house
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cat_house.data.HotelContract
 import com.example.cat_house.data.HotelDbHelper
@@ -28,9 +32,50 @@ class MainActivity : AppCompatActivity() {
         dbHelper = HotelDbHelper(this)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater(this).inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_insert_new_data) {
+            insertGuest()
+            displayDatabaseInfo()
+        }
+        if (item.itemId == R.id.action_insert_new_data_by_exec) {
+            insertGuestByExec()
+            displayDatabaseInfo()
+        }
+        return true
+    }
+
     override fun onStart() {
         super.onStart()
         displayDatabaseInfo()
+    }
+
+    private fun insertGuest() {
+        val db = dbHelper.writableDatabase
+
+        val values = ContentValues()
+        values.apply {
+            put(HotelContract.GuestEntry.COLUMN_NAME, "Murzik")
+            put(HotelContract.GuestEntry.COLUMN_CITY, "Murmansk")
+            put(HotelContract.GuestEntry.COLUMN_GENDER, HotelContract.GuestEntry.GENDER_MALE)
+            put(HotelContract.GuestEntry.COLUMN_AGE, 7)
+        }
+        db.insert(HotelContract.GuestEntry.TABLE_NAME, null, values)
+    }
+
+    private fun insertGuestByExec() {
+        val db = dbHelper.writableDatabase
+
+        val insertQuery =
+            "INSERT INTO ${HotelContract.GuestEntry.TABLE_NAME} " +
+                    "(${HotelContract.GuestEntry.COLUMN_NAME}, " +
+                    "${HotelContract.GuestEntry.COLUMN_CITY}) " +
+                    "VALUES ('Васька','Питер')"
+        db.execSQL(insertQuery)
     }
 
     private fun displayDatabaseInfo() {
@@ -44,7 +89,7 @@ class MainActivity : AppCompatActivity() {
             HotelContract.GuestEntry.COLUMN_AGE
         )
 
-        val selection = "${HotelContract.GuestEntry._ID} + >?"
+        val selection = "${HotelContract.GuestEntry._ID} >?"
         val selectionArgs = arrayOf("1")
 
         val cursor = db.query(
@@ -54,41 +99,44 @@ class MainActivity : AppCompatActivity() {
             selectionArgs,
             null,
             null,
-            "${HotelContract.GuestEntry.COLUMN_AGE} + DESC" )
+            "${HotelContract.GuestEntry._ID} DESC"
+        )
 
+        //use заменяте try{} - finally{cursor.close()}
+        cursor.use {
+            binding.displayTextView.text = "Таблица содержит ${it.count} гостей.\n\n"
 
-        try {
-            binding.displayTextView.text = "Таблица содержит ${cursor.count} гостей.\n\n"
-
-            binding.displayTextView.append("${HotelContract.GuestEntry._ID} -" +
-                    "${HotelContract.GuestEntry.COLUMN_NAME} -" +
-                    "${HotelContract.GuestEntry.COLUMN_CITY} - " +
-                    "${HotelContract.GuestEntry.COLUMN_GENDER} - " +
-                    "${HotelContract.GuestEntry.COLUMN_AGE} - ")
+            binding.displayTextView.append(
+                "${HotelContract.GuestEntry._ID} -" +
+                        "${HotelContract.GuestEntry.COLUMN_NAME} -" +
+                        "${HotelContract.GuestEntry.COLUMN_CITY} - " +
+                        "${HotelContract.GuestEntry.COLUMN_GENDER} - " +
+                        "${HotelContract.GuestEntry.COLUMN_AGE} - "
+            )
 
             binding.displayTextView.append("\n")
 
-            val idColumnIndex = cursor.getColumnIndex(HotelContract.GuestEntry._ID)
-            val nameColumnIndex = cursor.getColumnIndex(HotelContract.GuestEntry.COLUMN_NAME)
-            val cityColumnIndex = cursor.getColumnIndex(HotelContract.GuestEntry.COLUMN_CITY)
-            val genderColumnIndex = cursor.getColumnIndex(HotelContract.GuestEntry.COLUMN_GENDER)
-            val ageColumnIndex = cursor.getColumnIndex(HotelContract.GuestEntry.COLUMN_AGE)
+            val idColumnIndex = it.getColumnIndex(HotelContract.GuestEntry._ID)
+            val nameColumnIndex = it.getColumnIndex(HotelContract.GuestEntry.COLUMN_NAME)
+            val cityColumnIndex = it.getColumnIndex(HotelContract.GuestEntry.COLUMN_CITY)
+            val genderColumnIndex = it.getColumnIndex(HotelContract.GuestEntry.COLUMN_GENDER)
+            val ageColumnIndex = it.getColumnIndex(HotelContract.GuestEntry.COLUMN_AGE)
 
-            while (cursor.moveToNext()){
-                val currentID = cursor.getInt(idColumnIndex)
-                val currentName = cursor.getString(nameColumnIndex)
-                val currentCity = cursor.getString(cityColumnIndex)
-                val currentGender = cursor.getInt(genderColumnIndex)
-                val currentAge = cursor.getInt(ageColumnIndex)
+            while (it.moveToNext()) {
+                val currentID = it.getInt(idColumnIndex)
+                val currentName = it.getString(nameColumnIndex)
+                val currentCity = it.getString(cityColumnIndex)
+                val currentGender = it.getInt(genderColumnIndex)
+                val currentAge = it.getInt(ageColumnIndex)
 
-                binding.displayTextView.append(("\n" + currentID + " - " +
-                        currentName + " - " +
-                        currentCity + " - " +
-                        currentGender + " - " +
-                        currentAge))
+                binding.displayTextView.append(
+                    ("\n" + currentID + " - " +
+                            currentName + " - " +
+                            currentCity + " - " +
+                            currentGender + " - " +
+                            currentAge)
+                )
             }
-        } finally {
-            cursor.close()
         }
 
     }
